3a6ec838654a594c6d07524cb3bb74eaa10b6e96
markdaniel2021 <markdanielfox@gmail.com>
2021-07-29 15:50:42 -0300

    Projeto criado

---
+++ backend/src/main/java/com/iFrut/wsgastosnuvem/WsgastosnuvemApplication.java
@@ -0,0 +1,13 @@
+package com.iFrut.wsgastosnuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsgastosnuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsgastosnuvemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/iFrut/wsgastosnuvem/WsgastosnuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iFrut.wsgastosnuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsgastosnuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
