17a5a98b57d8b2540ecb9b84a57bc845cf49c2ea
markdaniel2021 <markdanielfox@gmail.com>
2021-07-29 22:37:19 -0300

    Componentes Estáticos Básicos adicionados

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,113 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>BillingPeriod</th>
+                        <th>Product/ServiceCode</th>
+                        <th>Product/ServiceOperation</th>
+                        <th>UsageType</th>
+                        <th>UsageAmount</th>
+                        <th>PricingUnit</th>
+                        <th>UnblendedCost</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AmazonEC2</td>
+                        <td>RunInstances</td>
+                        <td>SAE1-BoxUsage:t2.micro</td>
+                        <td>1</td>
+                        <td>Hrs</td>
+                        <td>$186.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/06/01 to 2021/07/01</td>
+                        <td>AWSCloudTrail</td>
+                        <td>None</td>
+                        <td>APS1-FreeEventsRecorded</td>
+                        <td>2</td>
+                        <td>Events</td>
+                        <td>$0.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/markdaniel2021" target="_blank" rel="noreferrer">Mark Daniel</a></p>
+                <p className="text-light"><strong>Desafio iFrut - Hackaton Ustore/Porto Digital - 2021</strong></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
